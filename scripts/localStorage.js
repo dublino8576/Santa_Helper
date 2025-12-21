4b0de383d75441316a7c2d049942df07e83ae405
[lucadm] <[lucadimaio@gmail.com]>
2025-12-21 12:35:04 +0000

    Save items to localStorage

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -3,13 +3,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const itemName = document.getElementById("item-name");
-    const itemImage = document.getElementById("item-image");
-    const itemDescription = document.getElementById("item-description");
-    localStorage.setItem("wishlistSubmitted", "true");
-    localStorage.setItem("itemName", itemName.value);
-    localStorage.setItem("itemImage", itemImage.value);
-    localStorage.setItem("itemDescription", itemDescription.value);
-    form.submit();
+
+    saveToLocalStorage();
   });
 });
+
+function saveToLocalStorage() {
+  const itemName = document.getElementById("item-name");
+  const itemImage = document.getElementById("item-image");
+  const itemDescription = document.getElementById("item-description");
+  const wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+  if (!itemName.value.trim()) {
+    document.getElementById("name-error").classList.remove("hidden");
+    return;
+  } else {
+    document.getElementById("name-error").classList.add("hidden");
+  }
+  console.log("i am here", wishlist.length);
+  if (wishlist.length === 8) {
+    alert("You can only save up to 8 wishlist items.");
+    return;
+  }
+  const newItem = {
+    name: itemName.value.trim(),
+    image: itemImage.value.trim(),
+    description: itemDescription.value.trim(),
+  };
+  wishlist.push(newItem);
+  localStorage.setItem("wishlist", JSON.stringify(wishlist));
+  console.log(localStorage.getItem("wishlist"), "localStorage");
+}
